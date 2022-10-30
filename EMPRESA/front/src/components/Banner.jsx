e0e5a0db7895d63449a34a8cebbfeec56d75e312
NextorJP <111332593+NextorJP@users.noreply.github.com>
2022-10-30 15:48:41 -0500

    Codigo borrado

--- EMPRESA/front/src/components/Banner.jsx
+++ EMPRESA/front/src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import img_banner_2 from '../images/img_banner_2.jpg'
-
+import img_banner_2 from '../images/img_banner_2.jpg'
 
 export const Banner = () => {
   return (
