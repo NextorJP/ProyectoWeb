a2826b6ee6f588acf0653cbcb9c26402e1df6dd7
NextorJP <111332593+NextorJP@users.noreply.github.com>
2022-10-31 13:28:16 -0500

    infoActualizada

---
+++ webmixtos/front/src/components/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Banner } from './Banner';
+import { ContentMenu } from './ContentMenu';
+import { Footer } from './Footer';
+
+export const Menu = () => {
+  return (
+    <>
+        <Banner/>
+        <ContentMenu/>
+        <Footer/>
+    </>
+  )
+}
